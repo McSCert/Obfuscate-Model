3b551c2160f5f86cc7361f0e7064ed0b22c88351
Monika Jaskolka <bialy.monika@gmail.com>
2018-03-08 15:54:55 +0000

    Renamed MathWorksSubmission to the generic SubmissionProcess
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@12251 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Remove/removeModelInformation.m
+++ Remove/removeModelInformation.m
@@ -4,15 +4,15 @@ function removeModelInformation(sys)
     date = datestr(now, 'ddd mmm dd HH:MM:SS yyyy');
     date = reshape(date, 1, length(date));
     name = 'user';
-    
+
     % Change usernames
-    set_param(sys, 'ModifiedByFormat', name); % Changes Creator and LastModifiedBy also, but only after saving
+    set_param(sys, 'ModifiedByFormat', name); % Changes LastModifiedBy also, but only after saving
     set_param(sys, 'Creator', name);
-    
+
     % Change dates
     set_param(sys, 'Created', date);
     set_param(sys, 'ModifiedDateFormat', date); % Changes LastModifiedDate also, but only after saving
     set_param(sys, 'ModifiedComment', '');
     set_param(sys, 'ModifiedHistory', '');
-    set_param(sys, 'ModelVersionFormat', '1.0'); % Changes ModelVersion also, but only after saving  
+    set_param(sys, 'ModelVersionFormat', '1.0'); % Changes ModelVersion also, but only after saving
 end
