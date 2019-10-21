a6294fd113918546ee5e96d3b7be4828af25da21
Monika Jaskolka <bialy.monika@gmail.com>
2019-10-21 13:57:59 +0000

    Now renames stateflow Events.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@18805 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Rename/renameStateflow.m
+++ Rename/renameStateflow.m
@@ -1,26 +1,32 @@
 function renameStateflow(sys)
-% RENAMESTATEFLOW Rename chart blocks to generic names.
+% RENAMESTATEFLOW Rename chart blocks and their data to generic names.
 
     rt = sfroot;
     model = rt.find('-isa', 'Simulink.BlockDiagram', '-and', 'Name', bdroot(sys));
     charts = model.find('-isa', 'Stateflow.Chart');
-    
+
     for i = 1:length(charts)
         c = charts(i);
-        
+
         % Rename chart
         c.Name = ['StateflowChart' num2str(i)];
-        
+
         % Rename inputs
         input_data = c.find('-isa', 'Stateflow.Data', 'Scope', 'Input');
         for j = 1:length(input_data)
             input_data(j).Name = ['Input' num2str(j)];
         end
-        
+
         % Rename outputs
         output_data = c.find('-isa', 'Stateflow.Data', 'Scope', 'Output');
         for k = 1:length(output_data)
             output_data(k).Name = ['Output' num2str(k)];
-        end 
+        end
+
+        % Rename events
+        output_data = c.find('-isa', 'Stateflow.Event');
+        for l = 1:length(events)
+            events(l).Name = ['Event' num2str(l)];
+        end
     end
 end
