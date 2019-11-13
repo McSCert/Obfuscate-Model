c92bacd96eea46876211a03faa33cfed46cf09cb
Monika Jaskolka <bialy.monika@gmail.com>
2019-11-13 23:30:08 +0000

    Fixed bugs that Gehan found.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@19113 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Rename/renameDSs.m
+++ Rename/renameDSs.m
@@ -1,21 +1,60 @@
 function renameDSs(sys)
-% RENAMEDSS Give all goto/froms generic Data Store Names. 
-% Note that this changes the "DataStoreName" parameter, not the "Name" parameter.
+% RENAMEDSS Give all Data Store Memory, Read, and Write blocks with generic
+% names. This changes the "DataStoreName" and "Name" parameters.
 % Does not take shadowing into account.
 
-    datastores = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreRead');
-     for i = 1:length(datastores)
-        datastorename  = get_param(datastores{i}, 'DataStoreName');
-     	writes = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreWrite', 'DataStoreName', datastorename);
-        reads = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreMemory', 'DataStoreName', datastorename);
+    % TODO: Error when blocks with the same name already exists
+    
+    datastores = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreMemory');
+    renamed_writes = [];
+    renamed_reads = [];
+    
+    for dsm = 1:length(datastores)
+        datastorename  = get_param(datastores{dsm}, 'DataStoreName');
+        writes = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreWrite', 'DataStoreName', datastorename);
+        reads  = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreRead', 'DataStoreName', datastorename);
         
-        set_param(datastores{i}, 'DataStoreName', ['DataStore' num2str(i)]);
+        % Memory
+        ds_name = ['DataStore' num2str(dsm)];
+        set_param(datastores{dsm}, 'DataStoreName', ds_name);
+        set_param(datastores{dsm}, 'Name', ['DataStoreMemory' num2str(dsm)]);
         
-        for j = 1:length(writes)
-            set_param(writes{j}, 'DataStoreName', ['DataStore' num2str(i)]);
+        % Write
+        if ~isempty(writes)
+            for w = 1:length(writes)
+                renamed_writes(end+1) = get_param(writes{w}, 'Handle');
+                set_param(writes{w}, 'DataStoreName', ds_name);
+                set_param(writes{w}, 'Name', ['DataStoreWrite' num2str(w)]);
+            end
         end
-        for j = 1:length(reads)
-            set_param(reads{j}, 'DataStoreName', ['DataStore' num2str(i)]);
+        
+        % Read
+        if ~isempty(reads)
+            for r = 1:length(reads)
+                renamed_reads(end+1) = get_param(reads{r}, 'Handle');
+                set_param(reads{r}, 'DataStoreName', ds_name);
+                set_param(reads{r}, 'Name', ['DataStoreRead' num2str(r)]);
+            end
         end
+    end
+    
+    % Fix dangline reads/writes
+    renamed_writes  = getfullname(renamed_writes);
+    renamed_reads   = getfullname(renamed_reads);
+    dangling_writes = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreWrite');
+    dangling_reads  = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreRead');
+    dangling_writes = setdiff(dangling_writes, renamed_writes);
+    dangling_reads  = setdiff(dangling_reads, renamed_reads);
+    
+    for dw = 1:length(dangling_writes)
+        dsm = dsm + 1;
+        set_param(dangling_writes{dw}, 'DataStoreName', ['DataStore' num2str(dsm)]);
+        set_param(dangling_writes{dw}, 'Name', ['DataStoreWrite' num2str(dw)]);
+    end
+    
+    for dr = 1:length(dangling_reads)
+        dsm = dsm + 1;
+        set_param(dangling_reads{dr}, 'DataStoreName', ['DataStore' num2str(dsm)]);
+        set_param(dangling_reads{dr}, 'Name', ['DataStoreRead' num2str(dr)]);
     end
 end
