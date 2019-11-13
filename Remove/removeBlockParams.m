5e17465850c81d5e83ba59ba9357a2cd1074d66c
Monika Jaskolka <bialy.monika@gmail.com>
2019-11-13 20:12:16 +0000

    Edits to removeBlockParams.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@19110 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Remove/removeBlockParams.m
+++ Remove/removeBlockParams.m
@@ -1,6 +1,6 @@
 function removeBlockParams(sys)
-% REMOVEBLOCKPARAMS Clear the any params on blocks that can contain text. 
-% Sometimes they reveal info.
+% REMOVEBLOCKPARAMS Clear any block parameters that can custom text
+% (e.g., 'Tag', 'Description', 'BlockDescription')
 
     allBlocks = find_system(sys, 'FindAll', 'on', 'FollowLinks', 'on', 'type', 'block');
     for i = 1:length(allBlocks)
@@ -13,13 +13,13 @@ function removeBlockParams(sys)
         end
         
         % Subsystem specific
-        if strcmp(get_param(allBlocks(i), 'BlockType'), 'SubSystem')
-            % RTW params
-            set_param(allBlocks(i), 'RTWSystemCode', 'Auto');
-            set_param(allBlocks(i), 'RTWFcnNameOpts', 'Auto');
-            set_param(allBlocks(i), 'RTWFcnName', '');
-            set_param(allBlocks(i), 'RTWFileNameOpts', 'Auto');
-            set_param(allBlocks(i), 'RTWFileName', '');
-        end
+%         if strcmp(get_param(allBlocks(i), 'BlockType'), 'SubSystem')
+%             % RTW params
+%             set_param(allBlocks(i), 'RTWSystemCode', 'Auto');
+%             set_param(allBlocks(i), 'RTWFcnNameOpts', 'Auto');
+%             set_param(allBlocks(i), 'RTWFcnName', '');
+%             set_param(allBlocks(i), 'RTWFileNameOpts', 'Auto');
+%             set_param(allBlocks(i), 'RTWFileName', '');
+%         end
     end
 end
