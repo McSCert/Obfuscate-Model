6ec0d139c86d41e08cbd1651dcd754faba876364
Monika Jaskolka <bialy.monika@gmail.com>
2019-11-14 02:06:36 +0000

    Added todo comment.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@19120 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Remove/removeCustomDataTypes.m
+++ Remove/removeCustomDataTypes.m
@@ -1,5 +1,9 @@
 function removeCustomDataTypes(sys)
 % REMOVECUSTOMDATATYPES Remove custom data types for inports.
+
+    % TODO: 
+    % - Allow fixdt
+    % - Should work on all blocks with a specified data type
     builtinTypes = [{'Inherit: auto'}, {'boolean'}, {'double'}, {'single'}, {'int8'}, {'uint8'}, {'int16'}, {'uint16'}, {'int32'}, {'uint32'}];
     inports = find_system(sys, 'FindAll', 'on', 'FollowLinks', 'on', 'type', 'block', 'BlockType', 'Inport');
     for i = 1:length(inports)
