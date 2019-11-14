d9785a4ad37c5f2d378c4da4eb69ebe17bcb7efc
Monika Jaskolka <bialy.monika@gmail.com>
2019-11-14 02:08:38 +0000

    Added optional parameters.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@19123 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Rename/renameStateflow.m
+++ Rename/renameStateflow.m
@@ -1,5 +1,19 @@
-function renameStateflow(sys)
+function renameStateflow(sys, varargin)
 % RENAMESTATEFLOW Rename chart blocks and their data to generic names.
+
+    % If not args are given, run all checks. 
+    % If some args are given, only run those enabled.
+    if isempty(varargin)
+        default = 1;
+    else
+        default = 0;
+    end
+
+    sfcharts = getInput('sfcharts', varargin, default);
+    sfports  = getInput('sfinputs', varargin, default);
+    sfevents = getInput('sfevents', varargin, default);
+    sfboxes  = getInput('sfboxes',  varargin, default);
+    sfstates = getInput('sfstates', varargin, default);
 
     rt = sfroot;
     model = rt.find('-isa', 'Simulink.BlockDiagram', '-and', 'Name', bdroot(sys));
@@ -8,52 +22,68 @@ function renameStateflow(sys)
     for i = 1:length(charts)
         c = charts(i);
 
-        % Rename chart
-        c.Name = ['StateflowChart' num2str(i)];
-
-        % Rename inputs
-        input_data = c.find('-isa', 'Stateflow.Data', 'Scope', 'Input');
-        for j = 1:length(input_data)
-            input_data(j).Name = ['Input' num2str(j)];
+        % Rename charts
+        if sfcharts
+            c.Name = ['StateflowChart' num2str(i)];
         end
+        
+        % Rename ports
+        if sfports
+            input_data = c.find('-isa', 'Stateflow.Data', 'Scope', 'Input');
+            for j = 1:length(input_data)
+                input_data(j).Name = ['Input' num2str(j)];
+            end
 
-        % Rename outputs
-        output_data = c.find('-isa', 'Stateflow.Data', 'Scope', 'Output');
-        for k = 1:length(output_data)
-            output_data(k).Name = ['Output' num2str(k)];
+            output_data = c.find('-isa', 'Stateflow.Data', 'Scope', 'Output');
+            for k = 1:length(output_data)
+                output_data(k).Name = ['Output' num2str(k)];
+            end
         end
 
         % Rename events
-        output_data = c.find('-isa', 'Stateflow.Event');
-        for l = 1:length(events)
-            events(l).Name = ['Event' num2str(l)];
+        if sfevents
+            events = c.find('-isa', 'Stateflow.Event');
+            for l = 1:length(events)
+                events(l).Name = ['Event' num2str(l)];
+            end
         end
-
-        % Boxes
-        boxes = c.find('-isa', 'Stateflow.Box');
-        grp = get(boxes, 'IsGrouped');
-
-        if numel(grp) > 1
-           isgrouped = cell2mat(grp);
-        else
-           isgrouped = grp;
+        
+        % Rename boxes
+        if sfboxes
+            boxes = c.find('-isa', 'Stateflow.Box');
+            for n = 1:length(boxes)
+                boxes(n).Name = ['Box' num2str(n)];
+            end
         end
-        set(boxes, 'IsGrouped', 0);
+        
+        %% Rename states
+        % Save parameter because it needs to be turned off
+        if sfstates
+            boxes = c.find('-isa', 'Stateflow.Box');
+            grp = get(boxes, 'IsGrouped');
 
-        states = c.find('-isa', 'Stateflow.State');
-        for m = 1:length(states)
-           states(m).Name = ['State' num2str(m)];
-        end
+            if numel(grp) > 1
+                isgrouped = cell2mat(grp);
+            else
+                isgrouped = grp;
+            end
+            set(boxes, 'IsGrouped', 0);
 
-        % Turn back on
-        for o = 1:length(boxes)
-           boxes(o).IsGrouped = isgrouped(o);
+            states = c.find('-isa', 'Stateflow.State');
+            for m = 1:length(states)
+                states(m).Name = ['State' num2str(m)];
+            end
+
+            % Turn back on
+            for o = 1:length(boxes)
+                boxes(o).IsGrouped = isgrouped(o);
+            end
         end
 
         %% Rename functions
         % Functions are used in transitions, etc. so its difficult to change
         %sf_functions = c.find('-isa', 'Stateflow.Function');
-
+        
         %% TODO: Annotations
 
     end
