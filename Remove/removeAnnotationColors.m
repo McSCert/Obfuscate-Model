a4a77bd890e26209e2b5465bb8a3c422066164a4
Monika Jaskolka <bialy.monika@gmail.com>
2019-11-14 02:05:34 +0000

    Fixed comment.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@19118 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Remove/removeAnnotationColors.m
+++ Remove/removeAnnotationColors.m
@@ -1,5 +1,5 @@
 function removeAnnotationColors(sys)
-% REMOVEANNOTATIONCOLORS Remove all block coloring from the model.
+% REMOVEANNOTATIONCOLORS Remove all annotation coloring from the model.
 
     ann = find_system(sys, 'FindAll', 'on', 'FollowLinks', 'on', 'type', 'annotation');
     for i = 1:length(ann)
