6337e04db402e4916098d896063dbab30d2e0710
Monika Jaskolka <bialy.monika@gmail.com>
2019-11-14 02:09:17 +0000

    Created an app for this tool.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Utilities/ObfuscateModel@19124 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Rename/renameBlock.m
+++
@@ -1,8 +0,0 @@
-function renameBlock(sys, name, newname)
-% RENAMEBLOCK Rename all blocks in a model with the same name to a new name.
-
-    blocks = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'Name', name);
-    for i = 1:length(blocks)
-        set_param(blocks{i}, 'Name', newname);
-    end
-end
--- obfuscateModel.m
+++ obfuscateModel.m
@@ -1,29 +1,132 @@
-function obfuscateModel(sys)
+function obfuscateModel(sys, varargin)
+% OBFUSCATEMODEL Obfuscate a Simulink model such that application-specific or
+% company-specific details are removed.
+%
+%   Inputs:
+%       sys         Model name.
+%       varargin    Parameter names and values for specifying what elements of
+%                   the model are affected.
+%
+%   Outputs:
+%       N/A
+%
+%   Side Effects:
+%       Modified model
+%
+    
+    % If not args are given, run all checks. 
+    % If some args are given, only run those enabled.
+    if isempty(varargin)
+        default = 1;
+    else
+        default = 0;
+    end
+    
+    %% Manage parameters
+    % Simulink
+    %   Remove
+    removemasks             = getInput('removemasks', varargin, default);
+    removelinks             = getInput('removelinks', varargin, default);
+    removesignalnames       = getInput('removesignalnames', varargin, default);
+    removedocblocks         = getInput('removedocblocks', varargin, default);
+    removeannotations       = getInput('removeannotations', varargin, default);
+    removedescriptions      = getInput('removedescriptions', varargin, default);
+    removeblockcallbacks    = getInput('removeblockcallbacks', varargin, default);
+    removemodelinformation  = getInput('removemodelinformation', varargin, default);
+    
+    removecolorblocks       = getInput('removecolorblocks', varargin, default);
+    removecolorannotations  = getInput('removecolorannotations', varargin, default);
+    
+    %   Rename
+    renamesubsystems        = getInput('renamesubsystems', varargin, default);
+    renameports             = getInput('renameports', varargin, default);
+    renameconstants         = getInput('renameconstants', varargin, default);
+    renamegotofromname      = getInput('renamegotofromname', varargin, default);
+    renamegotofromtag       = getInput('renamegotofromtag', varargin, default);
+    renamedatastorename     = getInput('renamedatastorename', varargin, default);
+    renametriggers          = getInput('renametriggers', varargin, default); 
+    
+    % Stateflow
+    sfcharts                = getInput('sfcharts', varargin, default);
+    sfports                 = getInput('sfports', varargin, default);
+    sfevents                = getInput('sfevents', varargin, default);
+    sfstates                = getInput('sfstates', varargin, default);
+    sfboxes                 = getInput('sfboxes', varargin, default);
+        
+    %% Perform obfuscation
+    % Remove parameters and blocks
+    if removemasks
+        removeMasks(sys)
+    end
+    
+    if removelinks
+        removeLinks(sys)
+    end
 
-    % Disable links
-    removeLinks(sys)
+    if removesignalnames
+        removeSignalNames(sys)
+    end
+    
+    if removedocblocks
+        removeDocBlocks(sys)
+    end
+    
+    if removeannotations
+        removeAnnotations(sys)
+    end
+    
+    if removedescriptions
+        removeDescriptions(sys)
+    end
 
-    % Delete blocks
-    removeAnnotations(sys)
-    removeDocBlocks(sys)
+    if removecolorblocks
+        removeBlockColors(sys)
+    end
+    
+    if removecolorannotations
+        removeAnnotationColors(sys)
+    end
 
-    % Remove parameters
-    removeMasks(sys)
-    removeColors(sys)
-    removeBlockParams(sys)     % might affect codegen
-    removeCustomDataTypes(sys) % will probably affect functionality
-    removeSignalNames(sys)
-    removeBlockFunctions(sys)
-    removeDescriptions(sys)
-    %removeModelInformation(sys) % doesnt work
+    if removeblockcallbacks
+        removeBlockCallbacks(sys)
+    end
+    
+    if removemodelinformation
+        removeModelInformation(sys)
+    end
+    
+    %removeCustomDataTypes(sys)  % will probably affect functionality
 
     % Rename
-    renameSubsystems(sys)
-    renameStateflow(sys)
-    renamePorts(sys)
-    renameConstants(sys)
-    renameGotoTags(sys)
-    renameGotos(sys)
-    renameDSs(sys)
-    renameLinks(sys)
+    if renamesubsystems
+        renameSubsystems(sys)
+    end
+    
+    if renameports
+        renamePorts(sys)
+    end
+    
+    if renameconstants
+        renameConstants(sys)
+    end
+    
+    if renamegotofromtag
+        renameGotoTags(sys)
+    end
+    
+    if renamegotofromname
+        renameGotos(sys)
+    end
+    
+    if renamedatastorename
+        renameDSs(sys)
+    end
+    
+    if renametriggers
+        renameTriggers(sys);
+    end
+    
+    renameStateflow(sys, 'sfcharts', sfcharts, 'sfports', sfports, 'sfevents', sfevents, 'sfstates', sfstates, 'sfboxes', sfboxes);
+    
+    %renameLinks(sys)
 end
