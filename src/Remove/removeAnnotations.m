49a1b3e2abce80bf76ced171e50c9bc8240054eb
Stephen Scott <stephen99scott@gmail.com>
2020-08-18 12:23:00 -0400

    Added functionality to remove block annotations

--- src/Remove/removeAnnotations.m
+++ src/Remove/removeAnnotations.m
@@ -2,4 +2,9 @@ function removeAnnotations(sys)
 % REMOVEANNOTATIONS Remove all annotations from the model. 
 % Removes any text, area, or image annotations.
     delete(find_system(sys, 'FindAll', 'on', 'FollowLinks', 'on', 'type', 'annotation'));
+    % Remove block annotations
+    blocks = Simulink.findBlocks(sys);
+    for i = 1:length(blocks)
+       set_param(blocks(i), 'AttributesFormatString', ''); 
+    end
 end
