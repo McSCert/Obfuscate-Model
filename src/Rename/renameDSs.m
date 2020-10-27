96727dc42d955bca2ec6ca111f6ff8f3b75daf25
Monika Jaskolka <bialy.monika@gmail.com>
2020-10-27 18:52:17 -0400

    Add function for all block name obfuscation
    Add the renameBlocks script, which handles all obfuscation dealing with the 'Name' parameter of blocks. This functionality was throughout several other functions. It was extracted and put in one function. The GUI was updated to remove the old functionality.

--- src/Rename/renameDSs.m
+++ src/Rename/renameDSs.m
@@ -1,10 +1,8 @@
 function renameDSs(sys)
 % RENAMEDSS Give all Data Store Memory, Read, and Write blocks with generic
-% names. This changes the "DataStoreName" and "Name" parameters.
+% names. This changes the "DataStoreName" parameter.
 % Does not take shadowing into account.
-
-    % TODO: Error when blocks with the same name already exists
-    
+   
     datastores = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'DataStoreMemory');
     renamed_writes = [];
     renamed_reads = [];
@@ -17,14 +15,12 @@ function renameDSs(sys)
         % Memory
         ds_name = ['DataStore' num2str(dsm)];
         set_param(datastores{dsm}, 'DataStoreName', ds_name);
-        set_param(datastores{dsm}, 'Name', ['DataStoreMemory' num2str(dsm)]);
-        
+ 
         % Write
         if ~isempty(writes)
             for w = 1:length(writes)
                 renamed_writes(end+1) = get_param(writes{w}, 'Handle');
                 set_param(writes{w}, 'DataStoreName', ds_name);
-                set_param(writes{w}, 'Name', ['DataStoreWrite' num2str(w)]);
             end
         end
         
@@ -33,7 +29,6 @@ function renameDSs(sys)
             for r = 1:length(reads)
                 renamed_reads(end+1) = get_param(reads{r}, 'Handle');
                 set_param(reads{r}, 'DataStoreName', ds_name);
-                set_param(reads{r}, 'Name', ['DataStoreRead' num2str(r)]);
             end
         end
     end
@@ -49,12 +44,10 @@ function renameDSs(sys)
     for dw = 1:length(dangling_writes)
         dsm = dsm + 1;
         set_param(dangling_writes{dw}, 'DataStoreName', ['DataStore' num2str(dsm)]);
-        set_param(dangling_writes{dw}, 'Name', ['DataStoreWrite' num2str(dw)]);
     end
     
     for dr = 1:length(dangling_reads)
         dsm = dsm + 1;
         set_param(dangling_reads{dr}, 'DataStoreName', ['DataStore' num2str(dsm)]);
-        set_param(dangling_reads{dr}, 'Name', ['DataStoreRead' num2str(dr)]);
     end
 end
