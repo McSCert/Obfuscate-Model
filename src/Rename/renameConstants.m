634b688b3a576d4a75678d7d0c59dd23e6621081
Monika Jaskolka <bialy.monika@gmail.com>
2020-10-30 11:29:26 -0400

    Do not rename numeric constants

--- src/Rename/renameConstants.m
+++ src/Rename/renameConstants.m
@@ -1,30 +1,20 @@
 function renameConstants(sys)
-% RENAMECONSTANTS Give all constants generic names. Should be run from the root
-% to avoid overlapping names.
-
+% RENAMECONSTANTS Give all constants generic values. Applies only if the
+% constant has a variable value instead of a number.
+    
     blocks = find_system(sys, 'LookUnderMasks', 'all', 'FollowLinks', 'on', 'BlockType', 'Constant');
     blocks = get_param(blocks, 'Handle');
-
+    
     for i = 1:length(blocks)
-        changed = false;
-
-        num = 1;
-        while ~changed
-            try
-                set_param(blocks{i}, 'Name', ['Constant' num2str(i)]);
-                changed = true;
-            catch
-                num = num + 1;
-            end
-        end
-
-        set_param(blocks{i}, 'Value', ['Constant' num2str(i)]);
-        set_param(blocks{i}, 'OutDataTypeStr', 'Inherit: Inherit from ''Constant value''');
-
-        try
-            set_param(blocks{i}, 'Mask', 'off');
-            set_param(blocks{i}, 'LibraryVersion', '');
-        catch
+        val = get_param(blocks{i}, 'Value');
+        isNaN = isnan(str2double(val));
+        
+        if isNaN
+            % Constant is a variable
+            set_param(blocks{i}, 'Value', ['Constant' num2str(i)]);
+            set_param(blocks{i}, 'OutDataTypeStr', 'Inherit: Inherit from ''Constant value''');
+            
+            % TODO: Should the workspace/data dictionary variable also be renamed?
         end
     end
 end
